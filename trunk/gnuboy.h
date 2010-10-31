c4070e9ac4eeb06723a64df50cc32d686561c7d3
clach04@gmail.com <clach04@gmail.com@7805c9f9-f40a-4a55-987e-badbe93f5060>
2010-10-31 22:37:19 +0000

    More compile warning fixes (related to missing prototypes).

--- trunk/gnuboy.h
+++ trunk/gnuboy.h
@@ -48,6 +48,20 @@ void loadstate(FILE *f); /* save.c */
 int unzip (const unsigned char *data, long *p, void (* callback) (unsigned char d)); /* inflate.c */
 
 int splitline(char **argv, int max, char *line); /* split.c */
+
+/* refresh.c */
+void refresh_1(byte *dest, byte *src, byte *pal, int cnt);
+void refresh_2(un16 *dest, byte *src, un16 *pal, int cnt);
+void refresh_3(byte *dest, byte *src, un32 *pal, int cnt);
+void refresh_4(un32 *dest, byte *src, un32 *pal, int cnt);
+void refresh_2_3x(un16 *dest, byte *src, un16 *pal, int cnt);
+void refresh_3_2x(byte *dest, byte *src, un32 *pal, int cnt);
+void refresh_3_3x(byte *dest, byte *src, un32 *pal, int cnt);
+void refresh_3_4x(byte *dest, byte *src, un32 *pal, int cnt);
+void refresh_4_2x(un32 *dest, byte *src, un32 *pal, int cnt);
+void refresh_4_3x(un32 *dest, byte *src, un32 *pal, int cnt);
+void refresh_4_4x(un32 *dest, byte *src, un32 *pal, int cnt);
+
 /*------------------------------------------*/
 
 #endif /* __GNUBOY_H__ */
